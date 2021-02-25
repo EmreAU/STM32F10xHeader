3411a985116c9841ec802c1f5fbac3629f4976cb
Emre <78263485+EmreAU@users.noreply.github.com>
2021-02-25 23:36:32 +0100

    Update stm32f103C8_gpio.cpp

--- stm32f103C8_gpio.cpp
+++ stm32f103C8_gpio.cpp
@@ -50,3 +50,36 @@ void GPIO_PinMode(GPIO_TypeDef *pGPIOx, uint16_t pinNumber, uint16_t pinMode, ui
 void GPIO_ResetPort(GPIO_TypeDef *pGPIOx){
 	pGPIOx->BSRR &= 0xFFFF;
 }
+
+bool GPIO_DigitalRead(GPIO_TypeDef *pGPIOx,uint16_t pinNumber){
+	return pGPIOx->IDR & pinNumber;
+}
+
+uint16_t GPIO_DigitalReadPort(GPIO_TypeDef *pGPIOx){
+	return pGPIOx->IDR;
+}
+
+void GPIO_DigitalWrite(GPIO_TypeDef *pGPIOx,uint16_t PinNumber, bool Value){
+	pGPIOx->ODR |= (Value << PinNumber);
+}	
+	
+void GPIO_DigitalWritePort(GPIO_TypeDef *pGPIOx,uint16_t Value){
+	pGPIOx->ODR |=  Value;
+}	
+	
+void GPIO_ToggleOutputPin(GPIO_TypeDef *pGPIOx,uint16_t PinNumber){
+	pGPIOx->ODR ^= (1 << PinNumber); // Pitaj profesora sta ova funkcija treba da radi 
+}
+
+
+void GPIO_AttachInterrupt(GPIO_TypeDef *pGPIOx, uint16_t PinNumber,Edge_Select Edge){
+
+}
+
+void GPIO_Interrupt_Enable(uint16_t PinNumber, IRQn_Type IrqNumber){
+	
+}
+
+void GPIO_Interrupt_Clear(uint16_t PinNumber){
+	
+}
